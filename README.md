4f17dd05c85a55ba6078990679ea084c1fbe4fef
Thorsten Kugelberg <tk@kugelberg.de>
2015-11-15 17:20:40 +0100

    Get it on

---
+++ README.md
@@ -0,0 +1,5 @@
+# ThorstenTest
+
+was auch immer ich testen will...
+
+
---
+++ Test/module.php
@@ -0,0 +1,27 @@
+<?
+class Ivona extends IPSModule
+{
+    
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        
+        //These lines are parsed on Symcon Startup or Instance creation
+        //You cannot use variables here. Just static values.
+    }
+    
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+        
+    }
+    
+    public function getMP3($text, $optinal = false)
+    {
+
+    }
+
+}
+?>
